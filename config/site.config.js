b6bbf56d5499e85d50c82b819d621c2f4f0a4b6b
oo_yang <52309935+ooyang0325@users.noreply.github.com>
2023-02-25 16:12:17 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -55,6 +55,10 @@ module.exports = {
       name: 'GitHub',
       link: 'https://github.com/ooyang0325',
     },
+    {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/oo___yang/',
+    }
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
